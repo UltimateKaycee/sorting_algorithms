8f6c7c140fdafd97714f9a17ff32d7bceeb9f46e
ultimatekaycee <ultimatekaycee@outlook.com>
2024-02-20 12:50:06 -0800

    Did a function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,29 @@
-#nclude "sort.h"
-#include <stdio.h>
-
-
+#include "sort.h"
 /**
- * selection_sort - Function to sort array of int in ascending
- * @array: Array
- * @size: Size of array
- *
- * Return: None
- */
+  * selection_sort - Function to sort array of int.
+  * @array: Array
+  * @size: size of Array
+  */
 void selection_sort(int *array, size_t size)
 {
-	size_t run, go, first;
-	int ven;
+	size_t run, bail, take, ven;
+	int *head;
 
-	for (go = 0; go < size - 1; go++)
+	if (!array || size < 2)
+		return;
+	head = array;
+	for (run = 0; run < size; run++, head++)
 	{
-		first = go;
-		for (run = go + 1; run < size; run++)
+		for (take = run, bail = run; bail < size; bail++)
+			if (array[bail] < array[take])
+			{
+				take = bail;
+			}
+		if (run != take)
 		{
-			if (array[run] < array[first])
-				first = run;
-		}
-
-		if (first != go)
-		{
-			ven = array[go];
-			array[go] = array[first];
-			array[first] = ven;
+			ven = array[take];
+			array[take] = *head;
+			*head = ven;
 			print_array(array, size);
 		}
 	}
